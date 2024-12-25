e68b3f7a37d01af62eb4fd746764ca85eae22254
Yosuke Mizutani <mogproj@gmail.com>
2024-12-24 22:18:48 -0700

    Add minimal header and footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 
-import { Container, Navbar } from "react-bootstrap";
+import { Container, Form, Navbar } from "react-bootstrap";
+import { APP_VERSION } from "../models/Context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faYoutube, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Header() {
   return (
     <header>
       <Navbar bg="light" data-bs-theme="light" className="border-bottom">
-        <Container>
+        <Container className="mx-3">
           <Navbar.Brand>Feign-Discord CSS Generator</Navbar.Brand>
         </Container>
+
+        <Form className="form-inline me-3">
+          <Container className="d-flex">
+            <small className="text-muted mt-2 me-3">v{APP_VERSION}</small>
+            <a className="nav-link me-3" href="https://www.youtube.com/@mogproject" target="_blank">
+              <FontAwesomeIcon icon={faYoutube} size="2x" />
+            </a>
+            <a className="nav-link" href="https://github.com/mogproject/feign-discord-css-generator/" target="_blank">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+          </Container>
+        </Form>
       </Navbar>
     </header>
   );
